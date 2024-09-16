bf1f07379f454fae76450360102763bacfb2b270
NADGATH-PY <juandaniel200607@gmail.com>
2024-09-16 13:29:21 -0400

    Añadido el script de detección de emociones y configuraciones de prueba

--- emotion_detection.py
+++ emotion_detection.py
@@ -3,12 +3,10 @@ import requests
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    payload = { "raw_document": {
-    "text": text_to_analyze}}
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code == 200:
-    return response.json().get("text")
-    else:
-    return f"Error: {response.status_code}, {response.text}"if __name__ == "__main__":
-    text = "I love this new technology."
-    print(emotion_detector(text))
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    try:
+        response = requests.post(url, json=input_json, headers=headers)
+        response.raise_for_status()  # Lanza un error para códigos de estado HTTP 4xx/5xx
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return f"Request failed: {e}"
